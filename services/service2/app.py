95708adc1df4dd6f9d2afcb9b139c2748658a9c2
avichayef <avichayef@gmail.com>
2025-03-12 20:27:53 +0200

    ddd

--- services/service2/app.py
+++ services/service2/app.py
@@ -8,5 +8,5 @@ def hello():
     return {"message": "Hello from Service 2!"}
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))  # Note: Different port from service1
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port)
